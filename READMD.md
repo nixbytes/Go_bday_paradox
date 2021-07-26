2625d4e406b0802ec30a17d87d4d734fd7640578
nixbytes <real8686@gmail.com>
2021-07-26 15:19:35 -0400

    add readme and print section

---
+++ READMD.md
@@ -0,0 +1,10 @@
+# Birthday Paradox
+
+This small project from the book ["The big Book of Small Python Projects"](https://inventwithpython.com/bigbookpython/), which I add my implementation of the project.
+
+### Example output
+
+All Code is under the Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0) 
+
+https://creativecommons.org/licenses/by-nc-sa/3.0/
+https://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
--- bdayPara/main.py
+++ bdayPara/main.py
@@ -35,3 +35,15 @@ def getMatch(birthdays):
             if birthdayA == birthdayB:
                 return birthdayA
 
+
+# Display the intro:
+print("""Birthday Paradox, by Al Sweigart al@inventwithpython.com
+
+The Birthday Paradox shows us that in a group of N people, the odds
+that two of them have matching birthdays is surprisingly large.
+This program does a Monte Carlo simulation (that is, repeated random
+simulations) to explore this concept.
+
+(It's not actually a paradox, it's just a surprising result.)
+ 47. 
+""")
